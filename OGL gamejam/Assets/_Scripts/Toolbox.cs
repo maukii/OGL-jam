72a1371add109054aaf58e792ba4f63807d03b15
maukii <maunokiiskinen@kamk.fi>
2019-08-02 22:00:51 +0300

    _Loader scene and scripts folder added

---
+++ OGL gamejam/Assets/_Scripts/Toolbox.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbox : MonoBehaviour
+{
+
+    // This can be used to get reference to all instances used
+    public static Toolbox Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
